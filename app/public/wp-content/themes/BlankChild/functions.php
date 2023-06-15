dd814d41965d5d05a991c15d243dc54c884e0ce7
Elisa Gruau <lisale.kurosaki@gmail.com>
2023-06-16 01:01:58 +0200

    suite responsive accueil

--- app/public/wp-content/themes/BlankChild/functions.php
+++ app/public/wp-content/themes/BlankChild/functions.php
@@ -4,8 +4,9 @@
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles(){
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-	wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . './css/page-commande.css');
-	wp_enqueue_style('header-style', get_stylesheet_directory_uri() . './css/header-style.css');
+	wp_enqueue_style('commander-style', get_stylesheet_directory_uri() . './css/commander.css');
+	wp_enqueue_style('header-style', get_stylesheet_directory_uri() . './css/header.css');
+	wp_enqueue_style('accueil-style', get_stylesheet_directory_uri() . './css/accueil.css');
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap' ); 
 }
 
